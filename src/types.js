7cf8394117673726535e799156e4609fff8215be
Héctor Menéndez <hmenendez@techlap.local>
2017-10-18 04:41:37 -0500

    Updated documentation.

--- src/types.js
+++ src/types.js
@@ -1,24 +1,21 @@
 /**
- * @module __types
- * @memberof Tools
+ * The subject passeed to the Mapper is not the expected type.
+ * @typedef {Error}
+ * @memberof Types
  */
+export const MapperParamError = {
+    name: 'MapperParamError',
+    message: 'Invalid parameter «%s». Expecting {%s}, got "%s"',
+};
+
+/**
+ * A map to all the properties contained in an object.
+ * @typedef {Object}
+ * @memberof Types
+ */
+export const MapperResult = {};
+
 export default {
-
-    /**
-    * The subject passeed to the Mapper is not the expected type.
-    * @typedef {Error}
-    * @memberof Tools.__types
-    */
-    MapperParamError: {
-        name: 'MapperParamError',
-        message: 'Invalid parameter «%s». Expecting {%s}, got "%s"',
-    },
-
-    /**
-    * A map to all the properties contained in an object.
-    * @typedef {Object}
-    * @memberof Tools.__types
-    */
-    MapperResult: {},
-
+    MapperParamError,
+    MapperResult,
 };
